312d0a8c73c6b74a081931dc2aac77f2655abe5f
hungnguyenquang221 <95635825+hungnguyenquang221@users.noreply.github.com>
2022-03-07 22:22:39 +0700

    upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Ngọc Nhi';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng người yêu anh';
+const contentCard = 'Chúc honey của anh 8-3 tràn ngập niềm vui và những nụ cười, luôn xinh đẹp. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Anh Yêu Em <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'z3234191999159_241617f7e0e4851820b71bd67827613e.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
